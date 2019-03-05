658889d56a4ea61ef3efcd523b59b757b4f63f9b
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-03-05 17:45:46 +0100

    Apply fixes from StyleCI (#4)

--- src/PhpResponsiveRandomQuoteServiceProvider.php
+++ src/PhpResponsiveRandomQuoteServiceProvider.php
@@ -4,15 +4,14 @@ namespace DavideCasiraghi\PhpResponsiveRandomQuote;
 
 use Illuminate\Support\ServiceProvider;
 use DavideCasiraghi\PhpResponsiveRandomQuote\Console\PhpResponsiveQuote;
-use DavideCasiraghi\PhpResponsiveRandomQuote\QuoteFactory;
 
 class PhpResponsiveRandomQuoteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if($this->app->runningInConsole()){
+        if ($this->app->runningInConsole()) {
             $this->commands([
-                PhpResponsiveQuote::class  //the console class
+                PhpResponsiveQuote::class,  //the console class
             ]);
         }
     }
