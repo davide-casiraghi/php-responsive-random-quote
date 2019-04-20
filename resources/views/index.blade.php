950f9f956af3236ecb9c6296789569fefc797259
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-20 11:41:27 +0200

    removed blockquote html tag

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-<blockquote class="quoteOfTheDay">
+<div class="quoteOfTheDay">
     <div class="text">
         <img class="start-quote" src="/vendor/responsive-quotes/assets/images/start-quote-teal.png" alt="Start Quote">
         {{--<div class="start-quote">"</div>--}}
@@ -7,4 +7,4 @@
         {{--<div class="end-quote">"</div>--}}
     </div>
     <cite>{{$quoteAuthor}}</cite>
-</blockquote>
+</div>
