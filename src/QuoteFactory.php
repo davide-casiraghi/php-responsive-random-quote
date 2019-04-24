e187e12eece44ac77c07b35ba95a2e7da4cc6b52
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-24 16:23:17 +0200

    refactored code

--- src/QuoteFactory.php
+++ src/QuoteFactory.php
@@ -6,18 +6,6 @@ namespace DavideCasiraghi\PhpResponsiveRandomQuote;
 
 class QuoteFactory
 {
-    /*const API_ENDPOINT = 'http://api.icndb.com/jokes/random/';
-    protected $client;*/
-
-    /*protected $quotes = array(
-        'Dr. Ida Rolf' => 'We want to get into the place where gravity reinforces and is a friend, a nourishing force.',
-        'Moshe Feldenkreis' => 'Another aspect of erect posture is that it is a biological quality of the human frame and there should be no sensation of any doing, holding, or effort whatsoever.',
-        'Michelangelo' => 'I saw the angel in the marble and carved until I set him free',
-        'Anonymus' => 'Energy flows where attention goes',
-        'Anonymus' => 'The battle is already decided before you start. Because all the movements are already in you.',
-        'Joseph Heller' => 'Depending on the structure of the body on which it acts, gravity can either support us and provide a springboard for our activities or it can pull at us and tear us down.',
-        'Lao Tsu' => 'All actions begins in rest.',
-    ); */
 
     protected $quotes = [
         0 => [
@@ -49,18 +37,9 @@ class QuoteFactory
         }
     }
 
-    /*public function __construct(Client $client = null)
-    {
-        $this->client = $client ?: new Client();
-    }*/
-
     public function getRandomQuote()
     {
         //dd($this->quotes[array_rand($this->quotes)]);
         return $this->quotes[array_rand($this->quotes)];
-
-        //$response = $this->client->get(self::API_ENDPOINT);
-        //$joke = json_decode($response->getBody()->getContents());
-        //return htmlspecialchars_decode($joke->value->joke);
     }
 }
