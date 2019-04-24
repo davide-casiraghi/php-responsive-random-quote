e382de4fdd83615278048f2ed1fdfd9bfacba4b6
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-24 11:49:22 +0200

    added edit link

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
     Route::group(['namespace' => 'DavideCasiraghi\PhpResponsiveRandomQuote\Http\Controllers', 'middleware' => 'web'], function () {
+        
+        /* Quotes */
         Route::resource('php-responsive-quote', 'ResponsiveQuoteController');
         Route::get('/random-quote/', 'ResponsiveQuoteController@showRandomQuote');
         
-        
+        /* Quote translations */
         Route::get('php-responsive-quote-translation/{quoteId}/{languageCode}/create', 'ResponsiveQuoteTranslationController@create')->name('php-responsive-quote-translation.create');
         Route::get('php-responsive-quote-translation/{quoteId}/{languageCode}/edit', 'ResponsiveQuoteTranslationController@edit')->name('php-responsive-quote-translation.edit');
         Route::resource('php-responsive-quote-translation', 'ResponsiveQuoteTranslationController')->except(['create','edit']);
