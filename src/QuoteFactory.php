764d1a714893e5c6f4ae0f8ff46a50f1e045e3e2
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-25 08:46:10 +0200

    adding getQuoteOfTheDay method

--- src/QuoteFactory.php
+++ src/QuoteFactory.php
@@ -6,8 +6,46 @@ use DavideCasiraghi\PhpResponsiveRandomQuote\Models\Quote;
 
 class QuoteFactory
 {
+    /***************************************************************************/
+    
+    /**
+     * Return a random quote
+     *
+     * @return \DavideCasiraghi\PhpResponsiveRandomQuote\Models\Quote
+     */
     public function getRandomQuote()
     {
         return Quote::inRandomOrder()->first();
+    }
+    
+    /***************************************************************************/
+    
+    /**
+     * Return the quote of the day 
+     *
+     * @return \DavideCasiraghi\PhpResponsiveRandomQuote\Models\Quote
+     */
+    public function getQuoteOfTheDay()
+    {
+        $numberOfQuotesInDB = Quote::count();
+        $quoteOfTheDayNumber = rand(1,$numberOfQuotesInDB);
+        
+        $quotes = Quote::all();
+        
+        return  $quotes[$quoteOfTheDayNumber];
+    }
+    
+    
+    
+    public function setCookie(Request $request){
+        $minutes = 60;
+        $response = new Response('Set Cookie');
+        $response->withCookie(cookie('quoteOfTheDayId', 'MyValue', $minutes));
+    return $response;
+    }
+
+    public function getCookie(Request $request){
+        $value = $request->cookie('quoteOfTheDayId');
+        echo $value;
     }
 }
